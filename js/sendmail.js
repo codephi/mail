982dd9d28bc6b764f31312085a296fe8bff36f15
PhilippeAssis <lippejc@gmail.com>
2015-04-04 22:17:19 -0300

    Commit

--- js/sendmail.js
+++ js/sendmail.js
@@ -1,60 +1,58 @@
-(function ($) {
-    $.fn.sendmail = function (custom) {
-        var merge = function (object, target) {
-            var newObject = {};
-            for (var key in object)
-                if (target[key] != undefined)
-                    newObject[key] = target[key];
+$.sendmail = $.fn.sendmail = function (custom) {
+    var merge = function (object, target) {
+        var newObject = {};
+        for (var key in object)
+            if (target[key] != undefined)
+                newObject[key] = target[key];
+            else
+                newObject[key] = object[key];
+
+        return newObject;
+    };
+
+    var optDefault = {
+        url: '',
+        success: function (response) {
+            if ($('.return', target).length) {
+                var targetReturn = $('.return', this);
+
+                if (targetReturn.is('input'))
+                    targetReturn.val(response.msg).addClass(response.success ? 'success' : 'error');
                 else
-                    newObject[key] = object[key];
+                    targetReturn.html(response.msg).addClass(response.success ? 'success' : 'error');
+            }
+            else if (response.msg)
+                alert(response.msg);
+            else
+                console.log(response);
 
-            return newObject;
-        };
-
-        var optDefault = {
-            url: '',
-            success: function (response) {
-                if ($('.return', target).length) {
-                    var targetReturn = $('.return', this);
-
-                    if (targetReturn.is('input'))
-                        targetReturn.val(response.msg).addClass(response.success ? 'success' : 'error');
+            if (response.captcha === true || response.error === true) {
+                if ($('.captcha', target).length) {
+                    if ($('.captcha', target).is('img'))
+                        $('.captcha', target).attr('src', options.url);
                     else
-                        targetReturn.html(response.msg).addClass(response.success ? 'success' : 'error');
+                        $('.captcha img', target).attr('src', options.url);
+
+                    $('input[name=captcha]', target).val('');
                 }
-                else if (response.msg)
-                    alert(response.msg);
-                else
-                    console.log(response);
+            }
+        },
+        dataType: 'json',
+        type: 'POST'
+    };
 
-                if (response.captcha === true || response.error === true) {
-                    if ($('.captcha', target).length) {
-                        if ($('.captcha', target).is('img'))
-                            $('.captcha', target).attr('src', options.url);
-                        else
-                            $('.captcha img', target).attr('src', options.url);
+    var options = (typeof custom == 'object') ? merge(optDefault, custom) : optDefault;
+    var target = $(this);
 
-                        $('input[name=captcha]', target).val('');
-                    }
-                }
-            },
-            dataType: 'json',
-            type: 'POST'
-        };
-
-        var options = (typeof custom == 'object') ? merge(optDefault, custom) : optDefault;
-        var target = $(this);
-
-        $(this).submit(function () {
-            var data = $(this).serializeArray();
-            $.ajax({
-                type: options.type,
-                url: options.url,
-                data: data,
-                success: options.success,
-                dataType: options.dataType
-            });
-            return false;
-        })
-    }
-})(jQuery);
+    $(this).submit(function () {
+        var data = $(this).serializeArray();
+        $.ajax({
+            type: options.type,
+            url: options.url,
+            data: data,
+            success: options.success,
+            dataType: options.dataType
+        });
+        return false;
+    })
+}
